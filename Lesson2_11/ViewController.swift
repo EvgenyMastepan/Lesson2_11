2437b9d9494e9e6952bff86b0a5962fc74f9effe
Evgeny Mastepan <phenikomperekt@gmail.com>
2024-12-21 17:49:30 +0700

    Is began

---
+++ Lesson2_11/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Lesson2_11
+//
+//  Created by Evgeny Mastepan on 21.12.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .red
+        
+    }
+
+
+}
+
